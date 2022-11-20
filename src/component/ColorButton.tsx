d0814fa20e39bf1c964760eb534fabb0d4cf17b2
Sean Long <sean.long.au@gmail.com>
2022-11-20 20:59:38 +1000

    Add copy color code functionality

--- src/component/ColorButton.tsx
+++ src/component/ColorButton.tsx
@@ -1,9 +1,20 @@
+import { useCallback } from "react";
 import { Button } from "@chakra-ui/react";
 import { MdContentCopy } from "react-icons/md";
 
 const ColorButton = ({ colorCode, label }: { colorCode: string, label: string }) => {
+  const copyToClipboard = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(colorCode);
+      console.log(`Copied '${colorCode}' to clipboard`);
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }, [colorCode]);
+
   return (
     <Button
+      onClick={copyToClipboard}
       variant='link'
       iconSpacing='1rem'
       colorScheme='blue'
